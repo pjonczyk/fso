a3d6084a5fa1e00d394af70934b1162031d2ddd4
Philipp Jonczyk <philjonczyk@gmail.com>
2021-08-06 16:35:07 +0200

    implemented 2.11

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Phonebook from './components/Phonebook'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: 4564564
-    }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
 
   const [currentFilter, setCurrentFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
 
   const handleNewName = (event) => {
     console.log(event.target.value)
